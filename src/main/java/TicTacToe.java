6a719d21cb596d720330fa50be4c3a1ab41b76da
TheRichCookie <amirsalehi138080@gmail.com>
2022-06-07 03:10:35 +0430

    I Add a AI to the Game

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,67 +1,89 @@
 import java.util.Scanner;
+import  java.util.Random;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class TicTacToe {
     Scanner Scan = new Scanner(System.in);
     String[] UI = {"1","2","3","4","5","6","7","8","9"};
     String Player = "X";
     int i = 0;
+    Random random = new Random();
+
+
 
 
     public static void main (String[] args){
         TicTacToe app = new TicTacToe();
         app.statements();
         app.Game();
+
+
     }
     public void Game(){
-        System.out.println(UI[0] + " " + UI[1] + " " + UI[2] + "\n" + UI[3] + " " + UI[4] + " " + UI[5] + "\n" + UI[6] + " " + UI[7] + " " + UI[8]);
-        System.out.println(Player + " Your Turn: " );
+        if(Player.equals("O")) {
+            System.out.println("***************************");
+            System.out.println(UI[0] + " " + UI[1] + " " + UI[2] + "\n" + UI[3] + " " + UI[4] + " " + UI[5] + "\n" + UI[6] + " " + UI[7] + " " + UI[8]);
+            System.out.println("***************************");
+            System.out.println(Player + " Your Turn: ");
+        }
         input();
         statements();
+
     }
     public void input(){
-        String Pinput = Scan.nextLine();
         System.out.println("***************************");
-        if (Pinput.equals(UI[0])) {
-            UI[0] = Player;
-            i++;
+        if(Player.equals("O")) {
+
+            String PlayerO = Scan.nextLine();
+            if (PlayerO.equals(UI[0])) {
+                UI[0] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[1])) {
+                UI[1] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[2])) {
+                UI[2] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[3])) {
+                UI[3] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[4])) {
+                UI[4] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[5])) {
+                UI[5] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[6])) {
+                UI[6] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[7])) {
+                UI[7] = Player;
+                i++;
+            } else if (PlayerO.equals(UI[8])) {
+                UI[8] = Player;
+                i++;
+            } else {
+                System.out.println("***************************");
+                System.out.println("Error");
+                System.out.println("***************************");
+                Game();
+            }
         }
-        else if (Pinput.equals(UI[1])) {
-            UI[1] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[2])) {
-            UI[2] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[3])) {
-            UI[3] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[4])) {
-            UI[4] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[5])) {
-            UI[5] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[6])) {
-            UI[6] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[7])) {
-            UI[7] = Player;
-            i++;
-        }
-        else if (Pinput.equals(UI[8])) {
-            UI[8] = Player;
-            i++;
+        else if(Player.equals("X"))
+        {
+
+            AI();
+
         }
         else {
+            System.out.println("***************************");
             System.out.println("Error");
             System.out.println("***************************");
             Game();
         }
+
+
     }
     public void swap(){
         if(Player.equals("X"))
@@ -141,4 +163,49 @@ public class TicTacToe {
             Game();
         }
     }
+    public void AI ()
+    {
+        String Pinput = String.valueOf(random.nextInt(1,9));
+        System.out.println("AI Round!");
+        if (Pinput.equals(UI[0])) {
+            UI[0] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[1])) {
+            UI[1] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[2])) {
+            UI[2] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[3])) {
+            UI[3] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[4])) {
+            UI[4] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[5])) {
+            UI[5] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[6])) {
+            UI[6] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[7])) {
+            UI[7] = Player;
+            i++;
+        }
+        else if (Pinput.equals(UI[8])) {
+            UI[8] = Player;
+            i++;
+        }
+        else {
+            AI();
+        }
+    }
+
 }
